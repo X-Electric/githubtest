d9f49fc431e85343fc2c60e2625a5cc660e7ef55
X-Electric <xelectric123@wp.pl>
2016-09-01 14:20:10 +0200

    My first commit

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("This is just a test.");
+            Console.WriteLine(sb);
+            Console.ReadKey();
+        }
+    }
+}
